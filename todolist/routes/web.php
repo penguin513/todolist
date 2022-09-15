c16770c8cfc9deaa6fb7de6e9b812a34dbb41b0e
penguin513 <penguin513@gmail.com>
2022-09-16 00:55:33 +0900

    create_folder

--- todolist/routes/web.php
+++ todolist/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\FolderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,6 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
+
+Route::post('/folders/create', [FolderController::class, 'create']);
