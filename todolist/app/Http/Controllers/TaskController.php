84af0a3ff0f042410ea8d628ea767f10238c6afe
penguin513 <penguin513@gmail.com>
2022-09-16 02:09:00 +0900

    create_task

--- todolist/app/Http/Controllers/TaskController.php
+++ todolist/app/Http/Controllers/TaskController.php
@@ -5,9 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\Folder;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
+    /**
+     * Topページ表示
+     * @param int $id
+     *
+     * @return view
+     */
     public function index(int $id) {
         $folders = Folder::all();
 
@@ -19,6 +26,41 @@ class TaskController extends Controller
             'folders' => $folders,
             'current_folder_id' => $current_folder->id,
             'tasks' => $tasks,
+        ]);
+    }
+
+
+    /**
+     * タスク追加ページ表示
+     * @param int $id
+     *
+     * @return view
+     */
+    public function showCreateForm(int $id)
+    {
+        return view('tasks.create', [
+            'folder_id' => $id
+        ]);
+    }
+
+
+    /**
+     * タスク追加ページ表示
+     * @param CreateTask $request, int $id
+     *
+     * @return redirect
+     */
+    public function create(int $id, CreateTask $request) {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
         ]);
     }
 }
--- todolist/routes/web.php
+++ todolist/routes/web.php
@@ -24,6 +24,11 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
+
+Route::post('/folders/{id}/tasks/create', [TaskController::class,'create']);
+
+
 Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
 
 Route::post('/folders/create', [FolderController::class, 'create']);
